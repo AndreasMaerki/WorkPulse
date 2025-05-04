10babc3eb7ff42bb6b0c238d2c684902bf53b729
Andreas Maerki <andreas.maerki@valora.com>
2025-05-04 12:58:36 +0200

    Initial Commit

---
+++ WorkPulse/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WorkPulse
+//
+//  Created by Andreas Maerki on 04.05.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WorkPulseTests/WorkPulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkPulseTests.swift
+//  WorkPulseTests
+//
+//  Created by Andreas Maerki on 04.05.2025.
+//
+
+import Testing
+@testable import WorkPulse
+
+struct WorkPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
